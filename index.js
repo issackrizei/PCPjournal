966c45c56be93cc086fad686eda8f9ffb2502c4e
Ismail Madani <152799817+issackrizei@users.noreply.github.com>
2025-07-30 22:22:53 +0200

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Mon Site avec Fond</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="contenu">
+    <!-- Logo ajouté ici -->
+    <img src="logo2.png" alt="Logo CPC" class="logo">
+
+    <h1>CPC - C'EST PAS COMPLIQUÉ</h1>
+    <p>L'INFORMATION FACILE POUR LES CONS, ET LES IDIOTS</p>
+
+    <div class="boutons">
+      <a href="https://substack.com/@pcpjournal" class="btn" target="_blank">Lire un article</a>
+      <a href="https://www.instagram.com/isma_mdny/" class="btn" target="_blank">Me suivre sur Instagram</a>
+    </div>
+  </div>
+</body>
+</html>
